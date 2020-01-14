a3b7fd4782211bcca9a4fa9cedd518f201cfc2f9
Oleg Barinov <oleg.barinov@nutanix.com>
2020-01-14 11:34:32 +0100

    clean up

--- src/canvas/renderAnnotations.js
+++
@@ -1,7 +0,0 @@
-// import icon from '../assets/annotation.svg';
-
-export default function(ctx, annotations) {
-  const icon = new Image();
-  icon.onload = () => annotations.map(a => ctx.drawImage(icon, a.x, a.y));
-  icon.src = '/annotation.svg';
-}
